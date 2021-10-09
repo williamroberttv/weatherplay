c3273ede655847ffedeb2a8b65651066b6c1483d
William Robert <willtaavares@gmail.com>
2021-10-09 02:00:12 -0300

    feat: add functions to format data and types

---
+++ src/utils/formatData.ts
@@ -0,0 +1,20 @@
+import { format } from 'date-fns';
+import { WeatherData } from './types';
+
+export const formatKelvinToCelsius = (
+  temperature: number,
+):number => (temperature - 273.15);
+
+export const formatData = (data: WeatherData): WeatherData => ({
+  ...data,
+  main: {
+    temp: formatKelvinToCelsius(data.main.temp),
+    temp_max: formatKelvinToCelsius(data.main.temp_max),
+    temp_min: formatKelvinToCelsius(data.main.temp_min),
+    humidity: data.main.humidity,
+  },
+  date: format(new Date(), 'dd/MM/yyyy'),
+});
+export function capitalizeFirstLetter(text: string) {
+  return text.charAt(0).toUpperCase() + text.slice(1);
+}
--- src/utils/types.ts
+++ src/utils/types.ts
@@ -1,10 +1,11 @@
+import { ReactNode } from 'react';
+
 export type Coordinates = {
   coords: {
     longitude: number;
     latitude: number;
   }
 }
-
 export type WeatherData = {
   weather: [{
     id: number;
@@ -12,6 +13,7 @@ export type WeatherData = {
     description: string;
     icon: string;
   }]
+  date: string;
   main: {
     temp: number;
     temp_min: number;
@@ -28,4 +30,57 @@ export type WeatherData = {
 }
 export type WeatherDetailsProps = {
   details: WeatherData[];
+}
+export type WeatherContextProps = {
+  getGenre: (number: number) => void;
+  getPlaylist: (arr: PlaylistData[]) => void;
+  getWeatherData: (data: WeatherData[]) => void;
+  handleInputValue: (text: string) => void;
+  inputValue: string;
+  genre: string;
+  playlist: PlaylistData[];
+  weatherData: WeatherData[];
+}
+export type WeatherProviderProps ={
+  children: ReactNode;
+}
+export type PlaylistResponse = {
+  tracks: {
+      hits: [
+       {
+         track:{
+
+           images:{
+             coverart: string;
+            },
+            url: string,
+            title: string,
+            subtitle: string,
+          }
+      },
+    ]
+
+  }
+}
+export type PlaylistData = {
+  track:{
+    images:{
+      coverart: string;
+     },
+     url: string,
+     title: string,
+     subtitle: string,
+   }
+}
+export type SearchProps = {
+  getWeatherByInputValue: () => void;
+}
+export type DisplayPlaylist = {
+  genre: string;
+  weather: WeatherData[];
+  myPlaylist: PlaylistData[];
+  id: string;
+}
+export type HeaderProps = {
+  getCoords: () => void;
 }
